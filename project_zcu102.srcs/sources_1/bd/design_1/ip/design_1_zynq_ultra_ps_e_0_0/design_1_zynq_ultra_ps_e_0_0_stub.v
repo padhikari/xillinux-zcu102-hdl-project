ce79593c2672ea28b343d0a8ce466cd2451aae66
Pabitra Adhikari <pabitraadhikari@gmail.com>
2020-06-18 09:49:20 -0700

    Initial project

---
+++ project_zcu102.srcs/sources_1/bd/design_1/ip/design_1_zynq_ultra_ps_e_0_0/design_1_zynq_ultra_ps_e_0_0_stub.v
@@ -0,0 +1,22 @@
+// Copyright 1986-2019 Xilinx, Inc. All Rights Reserved.
+// --------------------------------------------------------------------------------
+// Tool Version: Vivado v.2019.1 (lin64) Build 2552052 Fri May 24 14:47:09 MDT 2019
+// Date        : Wed Jun 17 15:42:30 2020
+// Host        : soccentric-ws2 running 64-bit Ubuntu 20.04 LTS
+// Command     : write_verilog -force -mode synth_stub
+//               /home/pabitra/workspace/project_zcu102/project_zcu102.srcs/sources_1/bd/design_1/ip/design_1_zynq_ultra_ps_e_0_0/design_1_zynq_ultra_ps_e_0_0_stub.v
+// Design      : design_1_zynq_ultra_ps_e_0_0
+// Purpose     : Stub declaration of top-level module interface
+// Device      : xczu9eg-ffvb1156-2-e
+// --------------------------------------------------------------------------------
+
+// This empty module with port declaration file causes synthesis tools to infer a black box for IP.
+// The synthesis directives are for Synopsys Synplify support to prevent IO buffer insertion.
+// Please paste the declaration into a Verilog source file or add the file as an additional source.
+(* X_CORE_INFO = "zynq_ultra_ps_e_v3_3_0_zynq_ultra_ps_e,Vivado 2019.1" *)
+module design_1_zynq_ultra_ps_e_0_0(pl_ps_irq0, pl_resetn0, pl_clk0)
+/* synthesis syn_black_box black_box_pad_pin="pl_ps_irq0[0:0],pl_resetn0,pl_clk0" */;
+  input [0:0]pl_ps_irq0;
+  output pl_resetn0;
+  output pl_clk0;
+endmodule
---
+++ project_zcu102.srcs/sources_1/bd/design_1/ip/design_1_zynq_ultra_ps_e_0_0/design_1_zynq_ultra_ps_e_0_0_stub.vhdl
@@ -0,0 +1,32 @@
+-- Copyright 1986-2019 Xilinx, Inc. All Rights Reserved.
+-- --------------------------------------------------------------------------------
+-- Tool Version: Vivado v.2019.1 (lin64) Build 2552052 Fri May 24 14:47:09 MDT 2019
+-- Date        : Wed Jun 17 15:42:30 2020
+-- Host        : soccentric-ws2 running 64-bit Ubuntu 20.04 LTS
+-- Command     : write_vhdl -force -mode synth_stub
+--               /home/pabitra/workspace/project_zcu102/project_zcu102.srcs/sources_1/bd/design_1/ip/design_1_zynq_ultra_ps_e_0_0/design_1_zynq_ultra_ps_e_0_0_stub.vhdl
+-- Design      : design_1_zynq_ultra_ps_e_0_0
+-- Purpose     : Stub declaration of top-level module interface
+-- Device      : xczu9eg-ffvb1156-2-e
+-- --------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity design_1_zynq_ultra_ps_e_0_0 is
+  Port ( 
+    pl_ps_irq0 : in STD_LOGIC_VECTOR ( 0 to 0 );
+    pl_resetn0 : out STD_LOGIC;
+    pl_clk0 : out STD_LOGIC
+  );
+
+end design_1_zynq_ultra_ps_e_0_0;
+
+architecture stub of design_1_zynq_ultra_ps_e_0_0 is
+attribute syn_black_box : boolean;
+attribute black_box_pad_pin : string;
+attribute syn_black_box of stub : architecture is true;
+attribute black_box_pad_pin of stub : architecture is "pl_ps_irq0[0:0],pl_resetn0,pl_clk0";
+attribute X_CORE_INFO : string;
+attribute X_CORE_INFO of stub : architecture is "zynq_ultra_ps_e_v3_3_0_zynq_ultra_ps_e,Vivado 2019.1";
+begin
+end;
